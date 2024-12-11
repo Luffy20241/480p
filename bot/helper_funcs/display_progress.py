36c0de83e9c0ca2cd23548ce1fd08b5b78bbf8e9
Luffy20241 <jobayerislam2017@gmail.com>
2024-12-11 17:53:30 +0600

    Update display_progress.py

--- bot/helper_funcs/display_progress.py
+++ bot/helper_funcs/display_progress.py
@@ -5,7 +5,7 @@
 # the logging things
 import logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.WARNING,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
